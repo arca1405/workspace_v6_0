c2b53155f2569ba4fdfc2e00922d4b49acc1d0dd
arca1405 <arca1983@live.com.mx>
2015-11-08 01:08:44 -0600

    UART Listo
    UART Funcionando

---
+++ UART_Echo/Debug/main.pp
@@ -0,0 +1,25 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/intrinsics.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/math.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/ymath.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/yvals.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/stdarg.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/linkage.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/_lock.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/float.h
+
+../main.c: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/intrinsics.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/math.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/ymath.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/_lock.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.3/include/float.h: 
